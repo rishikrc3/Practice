3425aa239878c4d53b510e6375fe2a1c31a944ce
Rishik Chakravarty <rishikrc3@gmail.com>
2023-07-27 15:47:18 +0530

    p_useState

--- practice/src/Content.js
+++ practice/src/Content.js
@@ -1,22 +1,37 @@
+import { useState } from "react";
+
 const Content = () => {
+  const [name, setName] = useState("Rishik");
+  const [count, setCount] = useState(0);
   const handleNameChange = () => {
-    const names = ["Bob", "Kevin", "Dave"];
+    const names = ["Bob", "Kevin", "Dave", "Rishik"];
     const int = Math.floor(Math.random() * 3);
-    return names[int];
+    setName(names[int]);
   };
 
   const handleClick = () => {
     alert("Fok has been clicked");
   };
 
+  const handleCounter = () => {
+    setCount(count + 1);
+  };
+  const handleResetcounter = () => {
+    setCount(0);
+  };
   function handleClick2(name) {
     alert(`${name} was clicked`);
   }
+  function refreshPage() {
+    window.location.reload();
+  }
   return (
     <main>
-      <p>Hello {handleNameChange()}!</p>
-      <button onClick={handleClick}>Fok it </button>
-      <button onClick={() => handleClick2("Rishik")}>Click fok</button>
+      <p>Hello {name}!</p>
+      <p>Counter {count}</p>
+      <button onClick={handleNameChange}>Change the foking name</button>
+      <button onClick={handleCounter}>Click to increase counter</button>
+      <button onClick={handleResetcounter}>Reset Counter</button>
     </main>
   );
 };

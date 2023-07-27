4bc02d30597ac8a6573563d5891ce0ffbce88148
Rishik Chakravarty <rishikrc3@gmail.com>
2023-07-28 00:53:38 +0530

    Prop_drilling

--- practice/src/Content.js
+++ practice/src/Content.js
@@ -1,31 +1,14 @@
-import { FaTrashAlt } from "react-icons/fa";
-
+import ItemList from "./ItemList";
+import React from "react";
 const Content = ({ items, handleCheck, handleDelete }) => {
   return (
     <main>
       {items.length ? (
-        <ul>
-          {items.map((item) => (
-            <li className="item" key={item.id}>
-              <input
-                type="checkbox"
-                onChange={() => handleCheck(item.id)}
-                checked={item.checked}
-              />
-              <label
-                style={item.checked ? { textDecoration: "line-through" } : null}
-                onDoubleClick={() => handleCheck(item.id)}
-              >
-                {item.item}
-              </label>
-              <FaTrashAlt
-                role="button"
-                tabIndex="0"
-                onClick={() => handleDelete(item.id)}
-              />
-            </li>
-          ))}
-        </ul>
+        <ItemList
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
       ) : (
         <p style={{ marginTop: "2rem" }}>Your list is empty.</p>
       )}

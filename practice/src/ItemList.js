40786c1d7f7b6c6764d9389972fdbe69a357be45
Rishik Chakravarty <rishikrc3@gmail.com>
2023-07-28 00:57:59 +0530

    even_more_prop_drilling

--- practice/src/ItemList.js
+++ practice/src/ItemList.js
@@ -1,27 +1,15 @@
 import React from "react";
-import { FaTrashAlt } from "react-icons/fa";
+
+import LineItem from "./LineItem";
 const ItemList = ({ items, handleCheck, handleDelete }) => {
   return (
     <ul>
       {items.map((item) => (
-        <li className="item" key={item.id}>
-          <input
-            type="checkbox"
-            onChange={() => handleCheck(item.id)}
-            checked={item.checked}
-          />
-          <label
-            style={item.checked ? { textDecoration: "line-through" } : null}
-            onDoubleClick={() => handleCheck(item.id)}
-          >
-            {item.item}
-          </label>
-          <FaTrashAlt
-            role="button"
-            tabIndex="0"
-            onClick={() => handleDelete(item.id)}
-          />
-        </li>
+        <LineItem
+          item={item}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
       ))}
     </ul>
   );

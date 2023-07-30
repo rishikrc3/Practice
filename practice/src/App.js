83842ddfcb4398d7c598e27c81046cec70de4fea
Rishik Chakravarty <rishikrc3@gmail.com>
2023-07-30 14:41:57 +0530

    final_cmmit

--- practice/src/App.js
+++ practice/src/App.js
@@ -53,11 +53,22 @@ function App() {
     const result = await apiRequest(API_URL, postOptions);
     if (result) setFetchError(result);
   };
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
     setItems(listItems);
+    const myItem = listItems.filter((item) => item.id === id);
+    const updateOptions = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ checked: myItem[0].checked }),
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    // if (result) setFetchError(result);
   };
 
   const handleDelete = async (id) => {
@@ -67,6 +78,7 @@ function App() {
     const reqUrl = `${API_URL}/${id}`;
     const result = await apiRequest(reqUrl, deleteOptions);
     if (result) setFetchError(result);
+    console.log(result);
   };
 
   const handleSubmit = (e) => {

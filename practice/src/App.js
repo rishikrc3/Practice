32b98665baf46855c9030aded61221f5e801faee
Rishik Chakravarty <rishikrc3@gmail.com>
2023-07-30 14:17:46 +0530

    delete_

--- practice/src/App.js
+++ practice/src/App.js
@@ -60,9 +60,13 @@ function App() {
     setItems(listItems);
   };
 
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
     const listItems = items.filter((item) => item.id !== id);
     setItems(listItems);
+    const deleteOptions = { method: "DELETE" };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, deleteOptions);
+    if (result) setFetchError(result);
   };
 
   const handleSubmit = (e) => {
